4e4b13ccbcc948a237c2f18b599d5c628c6e9c3e
Luis Henrique Barboza <124843843+BarbozahLuis@users.noreply.github.com>
2023-09-18 19:53:41 -0300

    Update scriptSema.js
    18/09

--- ProjetoGrupo/script/scriptSema.js
+++ ProjetoGrupo/script/scriptSema.js
@@ -13,6 +13,10 @@ const trafficLight = (event) => {
 // Basicamente, esses códigos fazem com que um elemento na página mude de cor em um 
 //ciclo predefinido sempre que a função changeColor é chamada.
 const nextIndex = () => colorIndex = colorIndex < 2 ? ++colorIndex : 0;
+/* a const next index e color index são responsaveis por definir a proxima cor do semáforo
+A função "changeColor" seleciona a cor correspondente 
+ao valor atual de "colorIndex" e chama a função correspondente para ligar a cor do semáforo.
+Em seguida, chama a função "nextIndex" para definir a próxima cor. */
 
 const changeColor = () => {
     const colors = ['red', 'yellow', 'green']
@@ -21,11 +25,13 @@ const changeColor = () => {
     nextIndex();
 }
 // Aqui é para deixar o semaforo automatico
-const stopAutomatic = () => {
+const stopAutomatic = () => {/*A função "stopAutomatic" interrompe a troca automática de cores do semáforo, 
+                             utilizando o identificador do intervalo de tempo armazenado em "intervalId". */
     clearInterval(intervalId);
 }
 
-// aqui ele está dando a função para poder trocar as cores do semaforo
+/* a variavel turnOn é um objeto que mapeia as cores do semáforo para Funções que ligam a cor 
+correspondente no semáforo */
 const turnOn = {
     'red': () => img.src = './img/vermelho.png',
     'yellow': () => img.src = './img/amarelo.png',
@@ -33,4 +39,5 @@ const turnOn = {
     'automatic': () => intervalId = setInterval(changeColor, 1000)
 }
 
-buttons.addEventListener('click', trafficLight);
+buttons.addEventListener('click', trafficLight);/*  aqui adicionei um addEventListener para que me 
+permita quando eu clicar nos botões ele faça a troca de cor do semáforo ou coloque ele em automático*/
